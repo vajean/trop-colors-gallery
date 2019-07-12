4f36354577f9fb5e0e05250581a4869cf9946695
Vajean <vajeans@gmail.com>
2019-07-13 00:02:22 +0100

    Bucket projects storage, functional

--- main.py
+++ main.py
@@ -15,32 +15,34 @@ app = Flask(__name__, static_folder='templates')
 
 @app.route('/<path:path>', methods=['GET'])
 def page(path):
-    requester = str(request.referrer).split('/').pop()
-    if requester != 'None' and requester != '':
-        pathKey = path.split('/').pop()
-        index = structure[requester][pathKey][1]
+    fullRequest = str(request.referrer).split('/')
+    project = fullRequest.pop()
+    if project != 'None' and project != '':
+        file = path.split('/').pop()
+        index = structure[project][file][1]
     else:
-        index = structure[path]['index.html'][1]
-        template = blobsList[index].download_as_string()
-        return render_template_string(template.decode('utf-8'))
+        if '/' in path:
+            fullPath = path.split('/')
+            file = fullPath.pop()
+            path = fullPath.pop()
+            project = fullPath.pop()
+            index = structure[project][file][1]
+        else:
+            if '.' in path:
+                return open('templates/projects/' + path, 'rb').read()
+            else:
+                index = structure[path]['index.html'][1]
+                template = blobsList[index].download_as_string()
+                return render_template_string(template.decode('utf-8'))
 
     return blobsList[index].download_as_string()
 
-#@app.route('/')
-# def root():
-    # For the sake of example, use static information to inflate the template.
-    # This will be replaced with real information in later steps.
 
- #   projects=getProjects()
-  #  return render_template('index.html', projects = projects)
+@app.route('/')
+def root():
+
+    return render_template('index.html', structure=structure)
 
 
 if __name__ == '__main__':
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
-    # Flask's development server will automatically serve static files in
-    # the "static" directory. See:
-    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
-    # App Engine itself will serve those files as configured in app.yaml.
     app.run(host='127.0.0.1', port=8080, debug=True)
